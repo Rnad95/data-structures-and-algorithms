3a28b6b4adba946a9a392ef26df3348b81bb9de3
Rnad95 <r.khawatrah13@gmail.com>
2022-03-08 23:00:55 +0300

    Ch07: Add KthFromEnd Method To Challenge06

---
+++ Challenge06+07/app/src/main/java/LTUCTest/linkedList.java
@@ -0,0 +1,132 @@
+package LTUCTest;
+
+import java.io.IOException;
+
+public class linkedList <T> {
+
+    Node Head;
+    public int count = 0;
+    public int i;
+    public linkedList() {
+        this.Head = null;
+
+    }
+
+    public void append(T value) {
+        Node newNode = new Node(value);
+        Node pointer = this.Head;
+        if (pointer == null) {
+            newNode.Next = this.Head;
+            this.Head = newNode;
+            ++count;
+        }
+        while (pointer != null) {
+            if (pointer.Next == null) {
+                pointer.Next = newNode;
+                newNode.Next = null;
+                ++count;
+                break;
+            }
+            pointer = pointer.Next;
+        }
+    }
+
+    public void insertAfter(T item, T value) {
+        Node pointer = this.Head;
+        Node newNode = new Node(value);
+        while (pointer != null) {
+
+            if (pointer.value == item) {
+                try {
+                    newNode.Next = pointer.Next;
+                    pointer.Next = newNode;
+                    ++count;
+                    break;
+                } catch (Exception err) {
+                    System.out.println(err.getMessage());
+                }
+
+            }
+            pointer = pointer.Next;
+
+        }
+    }
+
+    public void insertBefore(T item, T value) {
+        Node newNode = new Node(value);
+        Node pointer = this.Head;
+
+        if (pointer == null) {
+            newNode.Next = this.Head;
+            this.Head = newNode;
+        }
+
+        while (pointer != null) {
+            if (pointer.Next.value == item) {
+                try {
+                    newNode.Next = pointer.Next;
+                    pointer.Next = newNode;
+                    ++count;
+                    break;
+                } catch (Exception err) {
+                    System.out.println(err.getMessage());
+                }
+            }
+            pointer = pointer.Next;
+        }
+    }
+
+    public void insert(T value) {
+        Node newNode = new Node(value);
+        try {
+            newNode.Next = this.Head;
+            this.Head = newNode;
+            ++count;
+
+        } catch (Exception er) {
+            System.out.println(er.getMessage());
+        }
+    }
+
+    public int length() {
+        return count;
+    }
+
+    public void print() {
+        Node pointer = this.Head;
+        while (pointer != null) {
+            System.out.print(pointer.value + "\t");
+            pointer = pointer.Next;
+        }
+        System.out.println();
+    }
+
+    public int kthFromEnd(int k) {
+        Node pointer = this.Head;
+        int count1 = count-1;
+        try {
+    while (pointer != null) {
+        pointer.index = count1;
+        pointer = pointer.Next;
+        count1--;
+    }
+    Node pointer2 = this.Head;
+    while (pointer2 != null) {
+        if (pointer2.index == k) {
+            this.i = k;
+            return (int) pointer2.value;
+
+        }
+        pointer2 = pointer2.Next;
+    }
+        }catch (Exception err){
+    System.out.println(err.getMessage());
+}
+                return -1;
+    }
+
+
+    public int getI() {
+        return i;
+    }
+}
