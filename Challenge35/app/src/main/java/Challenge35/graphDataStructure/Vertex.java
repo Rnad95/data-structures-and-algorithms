03cdf798c23fa98cc72a40c5501269c52e49c3dc
Rnad95 <r.khawatrah13@gmail.com>
2022-05-24 14:32:10 +0300

    Graph data structure Implementation

---
+++ Challenge35/app/src/main/java/Challenge35/graphDataStructure/Vertex.java
@@ -0,0 +1,37 @@
+package Challenge35.graphDataStructure;
+
+import java.util.Objects;
+
+public class Vertex <T>{
+
+    T value;
+
+    public Vertex() {
+    }
+
+    public Vertex(T value) {
+        this.value = value;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return Objects.equals(value, vertex.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "value=" + value +
+                '}';
+    }
+}
